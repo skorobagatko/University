163f95b3f0da773f548473ace42d8ecd94420a8b
Stanislav Skorobahatko <skorobagatko.stanislav@gmail.com>
2020-11-23 21:32:18 +0200

    Refactor Lecture model

--- src/main/java/com/skorobahatko/university/domain/Lecture.java
+++ src/main/java/com/skorobahatko/university/domain/Lecture.java
@@ -6,15 +6,25 @@ import java.util.Objects;
 
 public class Lecture {
 	
-	private String courseId;
+	private int id;
+	private int courseId;
 	private String name;
 	private LocalDate date;
 	private LocalTime startTime;
 	private LocalTime endTime;
 	private int roomNumber;
 	
-	Lecture(String name, Course course, LocalDate date, LocalTime startTime, LocalTime endTime, int roomNumber) {
-		this.courseId = course.getId();
+	public Lecture(String name, LocalDate date, LocalTime startTime, LocalTime endTime, int roomNumber) {
+		this(0, name, 0, date, startTime, endTime, roomNumber);
+	}
+	
+	public Lecture(String name, int courseId, LocalDate date, LocalTime startTime, LocalTime endTime, int roomNumber) {
+		this(0, name, courseId, date, startTime, endTime, roomNumber);
+	}
+	
+	public Lecture(int id, String name, int courseId, LocalDate date, LocalTime startTime, LocalTime endTime, int roomNumber) {
+		this.id = id;
+		this.courseId = courseId;
 		this.name = name;
 		this.date = date;
 		this.startTime = startTime;
@@ -22,43 +32,59 @@ public class Lecture {
 		this.roomNumber = roomNumber;
 	}
 	
-	public String getCourseId() {
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getCourseId() {
 		return courseId;
+	}
+	
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
 	}
 	
 	public String getName() {
 		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public LocalDate getDate() {
 		return date;
 	}
 
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
 	public LocalTime getStartTime() {
 		return startTime;
+	}
+	
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
 	}
 
 	public LocalTime getEndTime() {
 		return endTime;
 	}
 
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+	
 	public int getRoomNumber() {
 		return roomNumber;
 	}
 	
-	void setDate(LocalDate date) {
-		this.date = date;
-	}
-	
-	void setStartTime(LocalTime startTime) {
-		this.startTime = startTime;
-	}
-	
-	void setEndTime(LocalTime endTime) {
-		this.endTime = endTime;
-	}
-
-	void setRoomNumber(int roomNumber) {
+	public void setRoomNumber(int roomNumber) {
 		this.roomNumber = roomNumber;
 	}
 
@@ -66,11 +92,12 @@ public class Lecture {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((courseId == null) ? 0 : courseId.hashCode());
+		result = prime * result + courseId;
 		result = prime * result + ((date == null) ? 0 : date.hashCode());
 		result = prime * result + ((endTime == null) ? 0 : endTime.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + roomNumber;
+		result = prime * result + id;
 		result = prime * result + ((startTime == null) ? 0 : startTime.hashCode());
 		return result;
 	}
@@ -89,18 +116,19 @@ public class Lecture {
 		
 		Lecture other = (Lecture) obj;
 		
-		return Objects.equals(name, other.name) &&
-				Objects.equals(courseId, other.courseId) &&
+		return id == other.id &&
+				roomNumber == other.roomNumber &&
+				courseId == other.courseId &&
+				Objects.equals(name, other.name) &&
 				Objects.equals(date, other.date) &&
 				Objects.equals(startTime, other.startTime) &&
-				Objects.equals(endTime, other.endTime) &&
-				roomNumber == other.roomNumber;
+				Objects.equals(endTime, other.endTime);
 	}
 
 	@Override
 	public String toString() {
-		return "Lecture [courseId=" + courseId + ", name=" + name + ", date=" + date + ", startTime=" + startTime
-				+ ", endTime=" + endTime + ", roomNumber=" + roomNumber + "]";
+		return "Lecture [id=" + id + ", courseId=" + courseId + ", name=" + name + ", date=" + date + ", startTime="
+				+ startTime + ", endTime=" + endTime + ", roomNumber=" + roomNumber + "]";
 	}
 
 }
