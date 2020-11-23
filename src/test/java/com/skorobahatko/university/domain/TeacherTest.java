5d44e719c2b298efbce95bc7bf627e7f4e1a5d6d
Stanislav Skorobahatko <skorobagatko.stanislav@gmail.com>
2020-11-23 21:37:16 +0200

    Refactor unit tests for Teacher model

--- src/test/java/com/skorobahatko/university/domain/TeacherTest.java
+++ src/test/java/com/skorobahatko/university/domain/TeacherTest.java
@@ -21,7 +21,7 @@ class TeacherTest {
 
 	@Test
 	void testAddCourse() {
-		Course course = new Course("QWERTY123", "Test course 123");
+		Course course = new Course(123, "Test course 123");
 		
 		assertTrue(!teacher.getCourses().contains(course));
 		
@@ -32,7 +32,7 @@ class TeacherTest {
 
 	@Test
 	void testRemoveCourse() {
-		Course course = new Course("QWERTY456", "Test course 456");
+		Course course = new Course(456, "Test course 456");
 		teacher.addCourse(course);
 		
 		teacher.removeCourse(course);
@@ -49,7 +49,7 @@ class TeacherTest {
 	
 	@Test
 	void testEqualsMethodReturnsFalseForNonEqualTeachers() {
-		long id = 123456789;
+		int id = 123456789;
 		String firstName = "Test";
 		String lastName = "Test";
 		List<Course> courses = getTestCourses();
@@ -61,30 +61,33 @@ class TeacherTest {
 	private List<Course> getTestCourses() {
 		List<Course> result = new ArrayList<>();
 		
-		Course course = new Course("TST-101", "Test course 1");
+		Course course = new Course(1, "Test course 1");
 		
+		int id = 1;
 		String lectureName = "Test lecture 1";
 		LocalDate lectureDate = LocalDate.of(2020, 11, 5);
 		LocalTime lectureStartTime = LocalTime.of(7, 30);
 		LocalTime lectureEndTime = LocalTime.of(9, 0);
 		int lectureRoomNumber = 101;
-		Lecture lecture = new Lecture(lectureName, course, lectureDate, lectureStartTime, lectureEndTime, lectureRoomNumber);
+		Lecture lecture = new Lecture(id, lectureName, course.getId(), lectureDate, lectureStartTime, lectureEndTime, lectureRoomNumber);
 		course.addLecture(lecture);
 		
+		id = 2;
 		lectureName = "Test lecture 2";
 		lectureDate = LocalDate.of(2020, 11, 6);
 		lectureStartTime = LocalTime.of(10, 30);
 		lectureEndTime = LocalTime.of(12, 0);
 		lectureRoomNumber = 201;
-		lecture = new Lecture(lectureName, course, lectureDate, lectureStartTime, lectureEndTime, lectureRoomNumber);
+		lecture = new Lecture(id, lectureName, course.getId(), lectureDate, lectureStartTime, lectureEndTime, lectureRoomNumber);
 		course.addLecture(lecture);
 		
+		id = 3;
 		lectureName = "Test lecture 3";
 		lectureDate = LocalDate.of(2020, 11, 7);
 		lectureStartTime = LocalTime.of(14, 30);
 		lectureEndTime = LocalTime.of(16, 0);
 		lectureRoomNumber = 301;
-		lecture = new Lecture(lectureName, course, lectureDate, lectureStartTime, lectureEndTime, lectureRoomNumber);
+		lecture = new Lecture(id, lectureName, course.getId(), lectureDate, lectureStartTime, lectureEndTime, lectureRoomNumber);
 		course.addLecture(lecture);
 		
 		result.add(course);
@@ -93,7 +96,7 @@ class TeacherTest {
 	}
 	
 	private Teacher getTestTeacher() {
-		long id = 1;
+		int id = 1;
 		String firstName = "John";
 		String lastName = "Johnson";
 		List<Course> courses = getTestCourses();
