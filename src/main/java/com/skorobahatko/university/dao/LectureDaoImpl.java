c6b46ad70e29680c0c27ddd502d7d932745b6a9a
Stanislav Skorobahatko <skorobagatko.stanislav@gmail.com>
2021-01-26 23:27:50 +0200

    Use Hibernate SessionFactory instead of Spring JdbcTemplate

--- src/main/java/com/skorobahatko/university/dao/LectureDaoImpl.java
+++ src/main/java/com/skorobahatko/university/dao/LectureDaoImpl.java
@@ -1,14 +1,16 @@
 package com.skorobahatko.university.dao;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
+import javax.persistence.NoResultException;
+import javax.transaction.Transactional;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.skorobahatko.university.dao.exception.DaoException;
 import com.skorobahatko.university.dao.exception.EntityNotFoundDaoException;
@@ -18,132 +20,96 @@ public class LectureDaoImpl implements LectureDao {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LectureDaoImpl.class);
 
-	private static final String LECTURE_ID = "lecture_id";
-	private static final String COURSE_ID = "course_id";
-	private static final String LECTURE_NAME = "lecture_name";
-	private static final String LECTURE_DATE = "lecture_date";
-	private static final String LECTURE_START_TIME = "lecture_start_time";
-	private static final String LECTURE_END_TIME = "lecture_end_time";
-	private static final String LECTURE_ROOM_NUMBER = "lecture_room_number";
+	private static final String GET_ALL_HQL = "from Lecture";
+
+	private static final String GET_BY_ID_HQL = "from Lecture l where l.id = :lId";
+
+	private static final String GET_BY_COURSE_ID_HQL = "from Lecture l where l.courseId = :courseId";
+
+	private static final String REMOVE_BY_COURSE_ID_HQL = "delete from Lecture l where l.courseId = :courseId";
 	
-	private static final String GET_ALL_SQL = "SELECT * FROM lectures";
+	private SessionFactory sessionFactory;
 
-	private static final String GET_BY_ID_SQL = "SELECT * FROM lectures WHERE lecture_id = ?";
-
-	private static final String GET_BY_COURSE_ID_SQL = "SELECT * FROM lectures WHERE course_id = ?";
-
-	private static final String ADD_SQL = "INSERT INTO lectures "
-			+ "(course_id, lecture_name, lecture_date, lecture_start_time, lecture_end_time, lecture_room_number) "
-			+ "VALUES (?, ?, ?, ?, ?, ?) RETURNING lecture_id;";
-	
-	private static final String UPDATE_SQL = "UPDATE lectures SET course_id = ?, lecture_name = ?, lecture_date = ?, "
-			+ "lecture_start_time = ?, lecture_end_time = ?, lecture_room_number = ? WHERE lecture_id = ?;";
-
-	private static final String REMOVE_BY_ID_SQL = "DELETE FROM lectures WHERE lecture_id = ?;";
-	
-	private static final String REMOVE_BY_COURSE_ID_SQL = "DELETE FROM lectures WHERE course_id = ?;";
-
-	private JdbcTemplate jdbcTemplate;
-
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
 	
 	@Override
+	@Transactional
 	public List<Lecture> getAll() {
 		logger.debug("Retrieving the lectures list");
 		
 		try {
-			List<Lecture> result = jdbcTemplate.query(GET_ALL_SQL, 
-					(rs, rowNum) -> {
-						int lectureId = rs.getInt(LECTURE_ID);
-						int courseId = rs.getInt(COURSE_ID);
-						String lectureName = rs.getString(LECTURE_NAME);
-						LocalDate lectureDate = rs.getDate(LECTURE_DATE).toLocalDate();
-						LocalTime lectureStartTime = rs.getTime(LECTURE_START_TIME).toLocalTime();
-						LocalTime lectureEndTime = rs.getTime(LECTURE_END_TIME).toLocalTime();
-						int lectureRoomNumber = rs.getInt(LECTURE_ROOM_NUMBER);
-						return new Lecture(lectureId, lectureName, courseId, lectureDate, lectureStartTime,
-								lectureEndTime, lectureRoomNumber);
-			});
+			Session session = sessionFactory.getCurrentSession();
+			List<Lecture> result = session.createQuery(GET_ALL_HQL, Lecture.class).getResultList();
 			
 			logger.debug("Retrieved {} lectures", result.size());
 			
 			return result;
-		} catch (DataAccessException e) {
+		} catch (HibernateException e) {
 			throw new DaoException("Unable to get lectures", e);
 		}
 	}
 
 	@Override
+	@Transactional
 	public Lecture getById(int id) {
 		logger.debug("Retrieving Lecture with id = {}", id);
 		
 		try {
-			Lecture result = jdbcTemplate.queryForObject(GET_BY_ID_SQL, new Object[] {id}, (rs, rowNum) -> {
-				int lectureId = rs.getInt(LECTURE_ID);
-				int courseId = rs.getInt(COURSE_ID);
-				String lectureName = rs.getString(LECTURE_NAME);
-				LocalDate lectureDate = rs.getDate(LECTURE_DATE).toLocalDate();
-				LocalTime lectureStartTime = rs.getTime(LECTURE_START_TIME).toLocalTime();
-				LocalTime lectureEndTime = rs.getTime(LECTURE_END_TIME).toLocalTime();
-				int lectureRoomNumber = rs.getInt(LECTURE_ROOM_NUMBER);
-				
-				return new Lecture(lectureId, lectureName, courseId, lectureDate, lectureStartTime,
-						lectureEndTime, lectureRoomNumber);
-			});
+			Session session = sessionFactory.getCurrentSession();
+			Query<Lecture> query = session.createQuery(GET_BY_ID_HQL, Lecture.class);
+			query.setParameter("lId", id);
+			Lecture result = query.getSingleResult();
 			
 			logger.debug("Lecture with id = {} successfully retrieved", id);
 			
 			return result;
-		} catch (EmptyResultDataAccessException e) {
+		} catch (NoResultException e) {
 			String message = String.format("Lecture with id = %d not found", id);
 			throw new EntityNotFoundDaoException(message, e);
-		} catch (DataAccessException e) {
+		} catch (HibernateException e) {
 			String message = String.format("Unable to get Lecture with id = %d", id);
 			throw new DaoException(message, e);
 		}
 	}
 
 	@Override
+	@Transactional
 	public List<Lecture> getByCourseId(int courseId) {
 		logger.debug("Retrieving Lecture for Course with id = {}", courseId);
 		
 		try {
-			List<Lecture> result = jdbcTemplate.query(GET_BY_COURSE_ID_SQL, ps -> ps.setInt(1, courseId), (rs, rowNum) -> {
-				int lectureId = rs.getInt(LECTURE_ID);
-				String lectureName = rs.getString(LECTURE_NAME);
-				LocalDate lectureDate = rs.getDate(LECTURE_DATE).toLocalDate();
-				LocalTime lectureStartTime = rs.getTime(LECTURE_START_TIME).toLocalTime();
-				LocalTime lectureEndTime = rs.getTime(LECTURE_END_TIME).toLocalTime();
-				int lectureRoomNumber = rs.getInt(LECTURE_ROOM_NUMBER);
-				return new Lecture(lectureId, lectureName, courseId, lectureDate, lectureStartTime, lectureEndTime,
-						lectureRoomNumber);
-			});
+			Session session = sessionFactory.getCurrentSession();
+			Query<Lecture> query = session.createQuery(GET_BY_COURSE_ID_HQL, Lecture.class);
+			query.setParameter("courseId", courseId);
+			List<Lecture> result = query.getResultList();
 			
 			logger.debug("Lectures list for Course with id = {} successfully retrieved", courseId);
 			
 			return result;
-		} catch (DataAccessException e) {
-			String message = String.format("Unable to get lectures with Course id = %d", courseId);
+		} catch (HibernateException e) {
+			String message = String.format("Unable to get lectures for Course with id = %d", courseId);
 			throw new DaoException(message, e);
 		}
 	}
 	
 	@Override
+	@Transactional
 	public void addAll(List<Lecture> lectures) {
 		lectures.forEach(this::add);
 	}
 
 	@Override
+	@Transactional
 	public void add(Lecture lecture) {
 		logger.debug("Adding Lecture: {}", lecture);
 		
 		try {
-			int lectureId = jdbcTemplate.queryForObject(ADD_SQL, Long.class, lecture.getCourseId(), lecture.getName(),
-					lecture.getDate(), lecture.getStartTime(), lecture.getEndTime(), lecture.getRoomNumber()).intValue();
+			Session session = sessionFactory.getCurrentSession();
+			int lectureId = (int) session.save(lecture);
 			lecture.setId(lectureId);
-		} catch (DataAccessException e) {
+		} catch (HibernateException e) {
 			String message = String.format("Unable to add Lecture: %s", lecture);
 			throw new DaoException(message, e);
 		}
@@ -152,13 +118,14 @@ public class LectureDaoImpl implements LectureDao {
 	}
 	
 	@Override
+	@Transactional
 	public void update(Lecture lecture) {
 		logger.debug("Updating Lecture: {}", lecture);
 		
 		try {
-			jdbcTemplate.update(UPDATE_SQL, lecture.getCourseId(), lecture.getName(), lecture.getDate(), 
-					lecture.getStartTime(), lecture.getEndTime(), lecture.getRoomNumber(), lecture.getId());
-		} catch (DataAccessException e) {
+			Session session = sessionFactory.getCurrentSession();
+			session.update(lecture);
+		} catch (HibernateException e) {
 			String message = String.format("Unable to update Lecture: %s", lecture);
 			throw new DaoException(message, e);
 		}
@@ -167,40 +134,37 @@ public class LectureDaoImpl implements LectureDao {
 	}
 
 	@Override
+	@Transactional
 	public void removeById(int id) {
 		logger.debug("Removing Lecture with id = {}", id);
 		
-		int affectedRows = 0;
-		
 		try {
-			affectedRows = jdbcTemplate.update(REMOVE_BY_ID_SQL, id);
-		} catch (DataAccessException e) {
+			Session session = sessionFactory.getCurrentSession();
+			Lecture lecture = session.byId(Lecture.class).load(id);
+			session.delete(lecture);
+		} catch (HibernateException e) {
 			String message = String.format("Unable to remove Lecture with id = %d", id);
 			throw new DaoException(message, e);
-		}
-		
-		if (affectedRows == 0) {
-			String message = String.format("Lecture with id = %d not found", id);
-			throw new EntityNotFoundDaoException(message);
 		}
 		
 		logger.debug("Lecture with id = {} successfully removed", id);
 	}
 	
 	@Override
+	@Transactional
 	public void removeByCourseId(int courseId) {
 		logger.debug("Removing Lectures with course id = {}", courseId);
 		
-		int affectedRows = 0;
 		try {
-			affectedRows = jdbcTemplate.update(REMOVE_BY_COURSE_ID_SQL, courseId);
-		} catch (DataAccessException e) {
-			String message = String.format("Unable to remove Lecture with id = %d", courseId);
+			Session session = sessionFactory.getCurrentSession();
+			int affectedRows = session.createQuery(REMOVE_BY_COURSE_ID_HQL)
+					.setParameter("courseId", courseId).executeUpdate();
+			
+			logger.debug("Removed {} lectures with course id = {}", affectedRows, courseId);
+		} catch (HibernateException e) {
+			String message = String.format("Unable to remove Lectures with course id = %d", courseId);
 			throw new DaoException(message, e);
 		}
-		
-		
-		logger.debug("Removed {} lectures with course id = {}", affectedRows, courseId);
 	}
 
 }
--- src/main/java/com/skorobahatko/university/dao/ParticipantDaoImpl.java
+++ src/main/java/com/skorobahatko/university/dao/ParticipantDaoImpl.java
@@ -2,16 +2,18 @@ package com.skorobahatko.university.dao;
 
 import java.util.List;
 
+import javax.persistence.NoResultException;
+import javax.transaction.Transactional;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.skorobahatko.university.dao.exception.DaoException;
 import com.skorobahatko.university.dao.exception.EntityNotFoundDaoException;
-import com.skorobahatko.university.domain.Course;
-import com.skorobahatko.university.domain.Lecture;
 import com.skorobahatko.university.domain.Participant;
 import com.skorobahatko.university.domain.Student;
 import com.skorobahatko.university.domain.Teacher;
@@ -20,34 +22,13 @@ public class ParticipantDaoImpl implements ParticipantDao {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ParticipantDaoImpl.class);
 
-	private static final String PARTICIPANT_ID = "participant_id";
-	private static final String FIRST_NAME = "first_name";
-	private static final String LAST_NAME = "last_name";
-	private static final String ROLE_NAME = "role_name";
-	private static final String COURSE_ID = "course_id";
-	private static final String COURSE_NAME = "course_name";
-
-	private static final String GET_ALL_SQL = "SELECT participant_id, first_name, last_name, participants.role_id, role_name "
-			+ "FROM participants INNER JOIN participant_roles ON participant_roles.role_id = participants.role_id;";
+	private static final String GET_ALL_HQL = "from Participant";
 	
-	private static final String GET_ALL_STUDENTS_SQL = "SELECT participant_id, first_name, last_name "
-			+ "FROM participants INNER JOIN participant_roles ON participant_roles.role_id = participants.role_id "
-			+ "WHERE participant_roles.role_name = 'Student';";
+	private static final String GET_ALL_STUDENTS_HQL = "from Student";
 	
-	private static final String GET_ALL_TEACHERS_SQL = "SELECT participant_id, first_name, last_name "
-			+ "FROM participants INNER JOIN participant_roles ON participant_roles.role_id = participants.role_id "
-			+ "WHERE participant_roles.role_name = 'Teacher';";
+	private static final String GET_ALL_TEACHERS_HQL = "from Teacher";
 
-	private static final String GET_BY_ID_SQL = "SELECT participants.first_name, participants.last_name, "
-			+ "participant_roles.role_name FROM participants "
-			+ "INNER JOIN participant_roles ON participant_roles.role_id = participants.role_id "
-			+ "WHERE participants.participant_id = ?;";
-
-	private static final String ADD_SQL = "INSERT INTO participants (first_name, last_name, role_id) "
-			+ "VALUES (?, ?, ?) RETURNING participant_id;";
-	
-	private static final String UPDATE_SQL = "UPDATE participants SET first_name = ?, "
-			+ "last_name = ? WHERE participant_id = ?;";
+	private static final String GET_BY_ID_HQL = "from Participant p where p.id = :pId";
 
 	private static final String ADD_PARTICIPANT_COURSE_PAIR_SQL = "INSERT INTO participants_courses "
 			+ "(participant_id, course_id) VALUES (?, ?);";
@@ -55,151 +36,100 @@ public class ParticipantDaoImpl implements ParticipantDao {
 	private static final String DELETE_PARTICIPANT_COURSE_PAIR_SQL = "DELETE FROM participants_courses "
 			+ "WHERE participant_id = ? AND course_id = ?;";
 
-	private static final String REMOVE_BY_ID_SQL = "DELETE FROM participants WHERE participant_id = ?;";
-
-	private static final String GET_PARTICIPANT_ROLE_ID_SQL = "SELECT role_id FROM participant_roles "
-			+ "WHERE role_name = ?;";
-
-	private static final String GET_COURSES_BY_PARTICIPANT_ID_SQL = "SELECT courses.course_id, courses.course_name FROM courses "
-			+ "INNER JOIN participants_courses ON participants_courses.course_id = courses.course_id "
-			+ "WHERE participants_courses.participant_id = ?;";
-
+	private static final String REMOVE_BY_ID_HQL = "delete from Participant p where p.id = :id";
 	
-
-	private JdbcTemplate jdbcTemplate;
-	private CourseDao courseDao;
-	private LectureDao lectureDao;
-
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-
-	public void setCourseDao(CourseDao courseDao) {
-		this.courseDao = courseDao;
-	}
-
-	public void setLectureDao(LectureDao lectureDao) {
-		this.lectureDao = lectureDao;
+	private SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
 
 	@Override
+	@Transactional
 	public List<Participant> getAll() {
 		logger.debug("Retrieving the participants list");
 		
 		try {
-			List<Participant> result = jdbcTemplate.query(GET_ALL_SQL, (rs, rowNum) -> {
-				int id = rs.getInt(PARTICIPANT_ID);
-				String firstName = rs.getString(FIRST_NAME);
-				String lastName = rs.getString(LAST_NAME);
-				String roleName = rs.getString(ROLE_NAME);
-				List<Course> courses = getParticipantCoursesByParticipantId(id);
+			Session session = sessionFactory.getCurrentSession();
+			List<Participant> result = session.createQuery(GET_ALL_HQL, Participant.class).getResultList();
 
-				Participant participant = getParticipantByRoleName(roleName);
-				participant.setId(id);
-				participant.setFirstName(firstName);
-				participant.setLastName(lastName);
-				participant.setCourses(courses);
-
-				return participant;
-			});
-			
 			logger.debug("Retrieved {} participants", result.size());
-			
+
 			return result;
-		} catch (DataAccessException e) {
+		} catch (HibernateException e) {
 			throw new DaoException("Unable to get participants", e);
 		}
 	}
 	
 	@Override
+	@Transactional
 	public List<Student> getAllStudents() {
 		logger.debug("Retrieving the Student list");
 		
 		try {
-			List<Student> result = jdbcTemplate.query(GET_ALL_STUDENTS_SQL, (rs, rowNum) -> {
-				int id = rs.getInt(PARTICIPANT_ID);
-				String firstName = rs.getString(FIRST_NAME);
-				String lastName = rs.getString(LAST_NAME);
-				List<Course> courses = getParticipantCoursesByParticipantId(id);
+			Session session = sessionFactory.getCurrentSession();
+			List<Student> result = session.createQuery(GET_ALL_STUDENTS_HQL, Student.class).getResultList();
 
-				return new Student(id, firstName, lastName, courses);
-			});
-			
 			logger.debug("Retrieved {} students", result.size());
-			
+
 			return result;
-		} catch (DataAccessException e) {
+		} catch (HibernateException e) {
 			throw new DaoException("Unable to get Student list", e);
 		}
 	}
 	
 	@Override
+	@Transactional
 	public List<Teacher> getAllTeachers() {
 		logger.debug("Retrieving the Teacher list");
 		
 		try {
-			List<Teacher> result = jdbcTemplate.query(GET_ALL_TEACHERS_SQL, (rs, rowNum) -> {
-				int id = rs.getInt(PARTICIPANT_ID);
-				String firstName = rs.getString(FIRST_NAME);
-				String lastName = rs.getString(LAST_NAME);
-				List<Course> courses = getParticipantCoursesByParticipantId(id);
+			Session session = sessionFactory.getCurrentSession();
+			List<Teacher> result = session.createQuery(GET_ALL_TEACHERS_HQL, Teacher.class).getResultList();
 
-				return new Teacher(id, firstName, lastName, courses);
-			});
-			
 			logger.debug("Retrieved {} teachers", result.size());
-			
+
 			return result;
-		} catch (DataAccessException e) {
+		} catch (HibernateException e) {
 			throw new DaoException("Unable to get Teacher list", e);
 		}
 	}
 
 	@Override
+	@Transactional
 	public Participant getById(int id) {
 		logger.debug("Retrieving Participant with id = {}", id);
 		
 		try {
-			Participant result = jdbcTemplate.queryForObject(GET_BY_ID_SQL, new Object[] {id}, (rs, rowNum) -> {
-				String firstName = rs.getString(FIRST_NAME);
-				String lastName = rs.getString(LAST_NAME);
-				String roleName = rs.getString(ROLE_NAME);
-				List<Course> courses = courseDao.getAllByParticipantId(id);
+			Session session = sessionFactory.getCurrentSession();
+			Query<Participant> query = session.createQuery(GET_BY_ID_HQL, Participant.class);
+			query.setParameter("pId", id);
+			Participant result = query.getSingleResult();
 
-				Participant participant = getParticipantByRoleName(roleName);
-				participant.setId(id);
-				participant.setFirstName(firstName);
-				participant.setLastName(lastName);
-				participant.setCourses(courses);
-				
-				return participant;
-			});
-			
 			logger.debug("Participant with id = {} successfully retrieved", id);
-			
+
 			return result;
-		} catch (EmptyResultDataAccessException e) {
+		} catch (NoResultException e) {
 			String message = String.format("Participant with id = %d not found", id);
 			throw new EntityNotFoundDaoException(message, e);
-		} catch (DataAccessException e) {
+		} catch (HibernateException e) {
 			String message = String.format("Unable to get Participant with id = %d", id);
 			throw new DaoException(message, e);
 		}
 	}
 
 	@Override
+	@Transactional
 	public void add(Participant participant) {
 		logger.debug("Adding Participant: {}", participant);
 		
 		checkForNull(participant);
-
+		
 		try {
-			int participantId = insertParticipant(participant);
-			
+			Session session = sessionFactory.getCurrentSession();
+			int participantId = (int) session.save(participant);
 			participant.setId(participantId);
-
-			insertParticipantCoursePairs(participant);
-		} catch (DataAccessException e) {
+		} catch (HibernateException e) {
 			String message = String.format("Unable to add Participant: %s", participant);
 			throw new DaoException(message, e);
 		}
@@ -208,15 +138,16 @@ public class ParticipantDaoImpl implements ParticipantDao {
 	}
 	
 	@Override
+	@Transactional
 	public void update(Participant participant) {
 		logger.debug("Updating Participant: {}", participant);
 		
 		checkForNull(participant);
 		
 		try {
-			jdbcTemplate.update(UPDATE_SQL, participant.getFirstName(), participant.getLastName(), 
-					participant.getId());
-		} catch (DataAccessException e) {
+			Session session = sessionFactory.getCurrentSession();
+			session.merge(participant);
+		} catch (HibernateException e) {
 			String message = String.format("Unable to update Participant: %s", participant);
 			throw new DaoException(message, e);
 		}
@@ -225,14 +156,20 @@ public class ParticipantDaoImpl implements ParticipantDao {
 	}
 
 	@Override
+	@Transactional
 	public void removeById(int id) {
 		logger.debug("Removing Participant with id = {}", id);
 		
 		int affectedRows = 0;
 		
 		try {
-			affectedRows = jdbcTemplate.update(REMOVE_BY_ID_SQL, id);
-		} catch (DataAccessException e) {
+			Session session = sessionFactory.getCurrentSession();
+			affectedRows = session.createQuery(REMOVE_BY_ID_HQL)
+					.setParameter("id", id)
+					.executeUpdate();
+
+			logger.debug("Participant with id = {} successfully removed", id);
+		} catch (HibernateException e) {
 			String message = String.format("Unable to remove Participant with id = %d", id);
 			throw new DaoException(message, e);
 		}
@@ -241,17 +178,20 @@ public class ParticipantDaoImpl implements ParticipantDao {
 			String message = String.format("Participant with id = %d not found", id);
 			throw new EntityNotFoundDaoException(message);
 		}
-		
-		logger.debug("Participant with id = {} successfully removed", id);
 	}
 	
 	@Override
+	@Transactional
 	public void addParticipantCourseById(int participantId, int courseId) {
 		logger.debug("Adding Course with id = {} to Participant with id = {}", courseId, participantId);
 		
 		try {
-			insertParticipantCoursePair(participantId, courseId);
-		} catch (DataAccessException e) {
+			Session session = sessionFactory.getCurrentSession();
+			session.createSQLQuery(ADD_PARTICIPANT_COURSE_PAIR_SQL)
+					.setParameter(1, participantId)
+					.setParameter(2, courseId)
+					.executeUpdate();
+		} catch (HibernateException e) {
 			String message = String.format("Unable to add Course with id = %d to Participant with id = %d", 
 					courseId, participantId);
 			throw new DaoException(message, e);
@@ -261,12 +201,17 @@ public class ParticipantDaoImpl implements ParticipantDao {
 	}
 
 	@Override
+	@Transactional
 	public void removeParticipantCourseById(int participantId, int courseId) {
 		logger.debug("Removing Course with id = {} for Participant with id = {}", courseId, participantId);
 		
 		try {
-			deleteParticipantCoursePair(participantId, courseId);
-		} catch (DataAccessException e) {
+			Session session = sessionFactory.getCurrentSession();
+			session.createSQLQuery(DELETE_PARTICIPANT_COURSE_PAIR_SQL)
+					.setParameter(1, participantId)
+					.setParameter(2, courseId)
+					.executeUpdate();
+		} catch (HibernateException e) {
 			String message = String.format("Unable to remove Course with id = %d for Participant with id = %d", 
 					courseId, participantId);
 			throw new DaoException(message, e);
@@ -275,67 +220,10 @@ public class ParticipantDaoImpl implements ParticipantDao {
 		logger.debug("Course with id = {} for Participant with id = {} successfully removed", courseId, participantId);
 	}
 
-	private int insertParticipant(Participant participant) {
-		int roleId = getParticipantRoleId(participant);
-		return jdbcTemplate
-				.queryForObject(ADD_SQL, Long.class, participant.getFirstName(), participant.getLastName(), roleId)
-				.intValue();
-	}
-
-	private void insertParticipantCoursePairs(Participant participant) {
-		List<Course> courses = participant.getCourses();
-		for (Course course : courses) {
-			insertParticipantCoursePair(participant.getId(), course.getId());
-		}
-	}
-	
-	private void insertParticipantCoursePair(int participantId, int courseId) {
-		jdbcTemplate.update(ADD_PARTICIPANT_COURSE_PAIR_SQL, participantId, courseId);
-	}
-	
-	private void deleteParticipantCoursePair(int participantId, int courseId) {
-		jdbcTemplate.update(DELETE_PARTICIPANT_COURSE_PAIR_SQL, participantId, courseId);
-	}
-
 	private void checkForNull(Participant participant) {
 		if (participant == null) {
 			throw new DaoException("The input argument must no be null");
 		}
-	}
-
-	private int getParticipantRoleId(Participant participant) {
-		return jdbcTemplate.query(GET_PARTICIPANT_ROLE_ID_SQL, ps -> {
-			String roleName = null;
-			if (participant instanceof Student) {
-				roleName = "Student";
-			} else if (participant instanceof Teacher) {
-				roleName = "Teacher";
-			}
-			ps.setString(1, roleName);
-		}, rs -> {
-			rs.next();
-			return rs.getInt("role_id");
-		});
-	}
-
-	private Participant getParticipantByRoleName(String roleName) {
-		if (roleName.equalsIgnoreCase("Teacher")) {
-			return new Teacher();
-		} else if (roleName.equalsIgnoreCase("Student")) {
-			return new Student();
-		} else {
-			throw new DaoException("Unable to get Participant role with name '" + roleName + "'");
-		}
-	}
-
-	private List<Course> getParticipantCoursesByParticipantId(int participantId) {
-		return jdbcTemplate.query(GET_COURSES_BY_PARTICIPANT_ID_SQL, ps -> ps.setInt(1, participantId),
-				(rs, rowNum) -> {
-					int courseId = rs.getInt(COURSE_ID);
-					String courseName = rs.getString(COURSE_NAME);
-					List<Lecture> courseLectures = lectureDao.getByCourseId(courseId);
-					return new Course(courseId, courseName, courseLectures);
-				});
 	}
 
 }
