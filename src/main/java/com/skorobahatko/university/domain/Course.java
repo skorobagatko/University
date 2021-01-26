99a3eff2f4de9c444d57a542362bcab5e25ddab3
Stanislav Skorobahatko <skorobagatko.stanislav@gmail.com>
2021-01-26 23:06:49 +0200

    Turn Course POJO class to the Hibernate Entity class

--- src/main/java/com/skorobahatko/university/domain/Course.java
+++ src/main/java/com/skorobahatko/university/domain/Course.java
@@ -4,10 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "courses")
 public class Course {
-	
+
+	@Id
+	@Column(name = "course_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+
+	@Column(name = "course_name")
 	private String name;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "course_id")
 	private List<Lecture> lectures;
 	
 	public Course() {}
