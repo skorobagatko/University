d1eb0b8019c930032cd892e3bc2f0e26d1dcc4b3
Stanislav Skorobahatko <skorobagatko.stanislav@gmail.com>
2021-01-27 20:38:28 +0200

    Remove unnecessary tests

--- src/test/java/com/skorobahatko/university/service/CourseServiceImplIT.java
+++ src/test/java/com/skorobahatko/university/service/CourseServiceImplIT.java
@@ -1,15 +1,14 @@
 package com.skorobahatko.university.service;
 
 import static com.skorobahatko.university.util.TestUtils.getTestCourse;
-import static com.skorobahatko.university.util.TestUtils.getTestParticipant;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -17,9 +16,7 @@ import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
-import com.skorobahatko.university.dao.ParticipantDao;
 import com.skorobahatko.university.domain.Course;
-import com.skorobahatko.university.domain.Participant;
 import com.skorobahatko.university.service.exception.EntityNotFoundServiceException;
 import com.skorobahatko.university.service.exception.ValidationException;
 
@@ -31,16 +28,14 @@ import com.skorobahatko.university.service.exception.ValidationException;
 })
 @Sql(scripts = "/delete_tables.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/applicationContext.xml")
+@ContextConfiguration("file:src/test/resources/springTestContext.xml")
 class CourseServiceImplIT {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	@Autowired
-	private ParticipantDao participantDao;
-	
-	@Autowired
+	@Qualifier("courseServiceImpl")
 	private CourseServiceImpl courseService;
 
 	@Test
@@ -48,21 +43,6 @@ class CourseServiceImplIT {
 		int expected = JdbcTestUtils.countRowsInTable(jdbcTemplate, "courses");
 		int actual = courseService.getAll().size();
 
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	void testGetAllByParticipantId() throws SQLException {
-		Course course = getTestCourse();
-		courseService.add(course);
-		
-		Participant participant = getTestParticipant();
-		participant.addCourse(course);
-		participantDao.add(participant);
-		
-		List<Course> expected = List.of(course);
-		List<Course> actual = courseService.getAllByParticipantId(participant.getId());
-		
 		assertEquals(expected, actual);
 	}
 
