fc1cf8a63eb64289ce1c664bdd4985f7fe28eb31
Stanislav Skorobahatko <skorobagatko.stanislav@gmail.com>
2021-01-27 21:37:09 +0200

    Change test database initialization to match the real one

--- src/test/resources/populate_timetables.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO timetables (participant_id, timetable_start_date, timetable_end_date) VALUES
-	(1, '2020-12-01', '2020-12-02'),
-	(2, '2020-12-03', '2020-12-03'),
-	(3, '2020-12-01', '2020-12-02');
